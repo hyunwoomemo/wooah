0426a641a56f9e672ba1530a2db8c31af44bca39
hyunwoomemo <archihw94@gmail.com>
2023-03-03 17:16:17 +0900

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,24 @@ import User from './pages/User';
 
 
 function App() {
+  let installPrompt = null;
 
+  useEffect(() => {
+    console.log('Listening for Install prompt');
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // For older browsers
+      e.preventDefault();
+      console.log('Install Prompt fired');
+      installPrompt = e;
+      // See if the app is already installed, in that case, do nothing
+      if (
+        (window.matchMedia && window.matchMedia('(display-mode: standalone)').matches) ||
+        window.navigator.standalone === true
+      ) {
+        return false;
+      }
+    });
+  }, []);
 
   const [loading, setLoading] = useState(true);
 
@@ -23,9 +40,30 @@ function App() {
     }, 2500)
   })
 
+  const installApp = async () => {
+    if (!installPrompt) {
+      alert('이미 다운로드 했습니다')
+      return false;
+    }
+
+    installPrompt.prompt();
+
+    let outcome = await installPrompt.userChoice;
+
+    if (outcome.outcome == 'accepted') {
+      console.log('App Installed');
+
+    } else {
+      console.log('App not installed');
+    }
+    // Remove the event reference
+    installPrompt = null;
+  };
+
   return (
     <Base>
       <GlobalStyle />
+      <button onClick={installApp}>추가</button>
       {loading ? <Loading /> :
         <>
           <Routes>
