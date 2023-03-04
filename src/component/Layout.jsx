c7960e34f5afea256768fdbf6e511474caf72add
hyunwoomemo <archihw94@gmail.com>
2023-03-04 23:43:48 +0900

    update

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -1,7 +1,9 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
 import NavigationBar from "./Navigation";
+import { AiOutlineReload } from "react-icons/ai";
 
 const Layout = ({ children }) => {
   const [touchPosition, setTouchPosition] = useState({});
@@ -12,12 +14,18 @@ const Layout = ({ children }) => {
     const distanceX = Math.abs(touchPosition.x - e.changedTouches[0].pageX);
     const distanceY = Math.abs(touchPosition.y - e.changedTouches[0].pageY);
 
-    if (distanceY + distanceX > 200 && distanceY > distanceX) {
+    if (distanceY + distanceX > 150 && distanceY > distanceX) {
       if (touchPosition.y - e.changedTouches[0].pageY < 0) {
         window.location.reload(true);
       }
     }
+
+    setTop(0);
   };
+
+  const [top, setTop] = useState(0);
+
+  const reloadRef = useRef();
 
   return (
     <Base
@@ -29,20 +37,17 @@ const Layout = ({ children }) => {
       }
       onTouchMove={(e) => {
         setReloadY(touchPosition.y - e.changedTouches[0].pageY);
-        if (reloadY < -200) {
-          setShowReload(true);
-        } else {
-          setShowReload(false);
+        if (reloadY < 0) {
+          setTop(Math.abs(reloadY));
         }
       }}
       onTouchEnd={touchEnd}
     >
       {showReload ? <Background></Background> : undefined}
-      {showReload ? (
-        <Reload reloadY={reloadY}>
-          <p>새로고침</p>
-        </Reload>
-      ) : undefined}
+      <Reload ref={reloadRef} reloadY={reloadY} top={top}>
+        <AiOutlineReload top={top} />
+        <p>당겨서 새로고침</p>
+      </Reload>
 
       <Header></Header>
       {children}
@@ -58,7 +63,6 @@ const Base = styled.div`
 const Background = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: #bebebe;
   position: absolute;
   z-index: 998;
   opacity: 0.5;
@@ -68,21 +72,31 @@ const Reload = styled.div`
   padding: 3rem;
   position: absolute;
   left: 50%;
-  transform: translateX(-50%);
+  height: 100%;
+  transform: translate(-50%, -100%);
+  width: 100%;
+  background-color: #fff;
   display: flex;
   justify-content: center;
   align-items: center;
   z-index: 999;
   margin: 0 auto;
   background-color: #ffffff;
-  border-radius: 50%;
+  top: ${({ top }) => (top > 0 ? `${top}px` : undefined)};
+
+  > svg {
+    font-size: 36px;
+    transform: ${({ top }) => (top > 0 ? `rotate(${top * 2}deg)` : undefined)};
+    color: skyblue;
+    opacity: ${({ top }) => (top > 50 ? `${top / 150}` : undefined)};
+    position: absolute;
+    bottom: 50px;
+  }
 
   > p {
     position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    white-space: nowrap;
+    bottom: 15px;
+    display: ${({ top }) => (top > 150 ? "block" : "none")};
   }
 `;
 
