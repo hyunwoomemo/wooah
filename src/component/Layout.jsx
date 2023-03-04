f7901379ee863c77753677898b8ab4d0e5ab0212
hyunwoomemo <archihw94@gmail.com>
2023-03-05 01:04:47 +0900

    update

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -68,29 +68,31 @@ const Background = styled.div`
 `;
 
 const Reload = styled.div`
-  padding: 3rem;
+  /* padding: 3rem; */
   position: absolute;
   left: 50%;
-  height: 100%;
+  bottom: 110%;
   transform: translateX(-50%);
-  bottom: 100%;
-  width: 100%;
   background-color: #fff;
   display: flex;
   justify-content: center;
   align-items: center;
   z-index: 999;
   margin: 0 auto;
-  background-color: #ffffff;
+  /* background-color: #9c8787; */
   bottom: ${({ top }) => (top > 0 ? `${100 - top / 10}%` : undefined)};
   /* transform: ${({ top }) => (top > 0 ? `translate(-50%, ${top - 100}%)` : undefined)}; */
   /* transition: all 0.3s; */
+  border-radius: 50%;
 
   > svg {
+    position: absolute;
+    top: 50%;
+    left: 50%;
     font-size: 36px;
-    transform: ${({ top }) => (top > 0 ? `rotate(${top * 2}deg)` : undefined)};
-    color: skyblue;
-    opacity: ${({ top }) => (top > 50 ? `${top / 150}` : undefined)};
+    transform: translate(-50%, -50%) ${({ top }) => (top > 0 ? `rotate(${top * 4}deg)` : undefined)};
+    color: ${({ top }) => (top > 149 ? `#5abde4` : "gray")};
+    opacity: ${({ top }) => (top > 0 ? `${top / 100}` : undefined)};
     position: absolute;
     bottom: 50px;
   }
