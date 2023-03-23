4113731b575e381b4807ea0fd260d877a6e22f8d
hyunwoomemo <archihw94@gmail.com>
2023-03-24 05:43:09 +0900

    update

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -22,6 +22,31 @@ const Layout = ({ children, main }) => {
     });
   });
 
+  const body = document.querySelector("body");
+  const scrollPosition = window.pageYOffset;
+
+  useEffect(() => {
+    if (openCategory || updateCategory) {
+      body.style.overflow = "hidden";
+      /* body.style.pointerEvents = "none"; */
+      body.style.position = "fixed";
+      body.style.top = `-${scrollPosition}px`;
+      body.style.left = "0";
+      body.style.right = "0";
+    }
+
+    return () => {
+      body.style.removeProperty("overflow");
+      body.style.removeProperty("pointer-events");
+      body.style.removeProperty("position");
+      body.style.removeProperty("top");
+      body.style.removeProperty("left");
+      body.style.removeProperty("right");
+
+      window.scrollTo(0, scrollPosition);
+    };
+  }, [openCategory, updateCategory]);
+
   return (
     <MilkModalContext.Provider value={{}}>
       <DateContext.Provider value={{ now, setNow }}>
