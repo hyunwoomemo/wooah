fe80160d154d065ad2767ba8e1ca8db1affc903f
hyunwoomemo <archihw94@gmail.com>
2023-04-01 06:20:31 +0900

    update

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
     }
 
     axios
-      .post("http://wooah.site:8080/login", {
+      .post("http://13.115.96.189:8080/login", {
         groupName: group,
         password: password,
       })
